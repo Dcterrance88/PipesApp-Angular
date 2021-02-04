114bf1cf35eb9c3e83233860343e9f7733a0ed45
Dcterrance88 <>
2021-02-04 12:03:56 -0500

    Añadiendo i18nPlurar pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,14 +6,24 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent  {
-  
-  public nombre : string = 'terrance';
-  public genero : string = 'masculino';
+export class NoComunesComponent {
+  //i18nSelect
+  public nombre: string = 'terrance';
+  public genero: string = 'masculino';
+  //i18nPlural
+  public clientes: string[] = ['Juan', 'carlos', 'luis', 'maria'];
 
+  //i18nSelect
   invitacionMapa = {
-    'masculino' : 'invitarlo',
-    'femenino' : 'invitarla'
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
 
+  //i18nPlural
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1' : 'tenemos un cliente esperando.',
+    '=2' : 'tenemos dos clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
 }
