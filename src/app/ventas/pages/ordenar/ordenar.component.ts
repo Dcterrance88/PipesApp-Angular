dff66f580c81f69c71e372072185991a633d4279
Dcterrance88 <>
2021-02-06 20:13:09 -0500

    Añadiendo valores a la tabla con pipes personalizados
    se crearon datos para poblar la tabla y dos pipes para modificar los
    datos y ordenarlos.

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,14 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
   public cambioMayuscula : boolean = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+  ]
 
   public cambio() : void {
     this.cambioMayuscula = !this.cambioMayuscula;
---
+++ src/app/ventas/pipes/vuela.pipe.ts
@@ -0,0 +1,13 @@
+
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: 'vuela'
+})
+export class VuelaPipe implements PipeTransform {
+
+  transform( vuela: boolean ): string {
+    return (vuela) ? 'puede volar' : 'no puede volar';
+  }
+
+}
